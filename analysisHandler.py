eaa763d15ba674e6f5da45dbaa79c203bc0bc36a
Mikkel Lauritzen <mikkel.hlauritzen@gmail.com>
2020-06-02 21:08:26 +0100

    cleanup

--- analysisHandler.py
+++
@@ -1,5 +0,0 @@
-import boto3
-
-def analyser(event, context):
-  comprehend = boto3.client("comprehend")
-  return comprehend.detect_sentiment(Text = event, LanguageCode = "en")
--- handler.js
+++ handler.js
@@ -6,7 +6,7 @@ const AWS = require('aws-sdk');
 const client = new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'});
 
 module.exports.bbcScraper = async event => {
-  const lambda  = new AWS.Lambda({region: 'eu-west-1'});
+  const comprehend = new AWS.Comprehend();
   const URL = 'https://bbc.co.uk';
   const response = await got(URL);
   const dom = new JSDOM(response.body);
@@ -16,21 +16,14 @@ module.exports.bbcScraper = async event => {
   const headlineElementsArr = [...headlineElements];
   const length = headlineElements.length >= MAX_LENGTH ? MAX_LENGTH : headlineElements.length;
   
-  let topHeadlines = headlineElementsArr.slice(0,length).map(e => e.textContent);
-  let headlinesStrng = JSON.stringify(topHeadlines.join('. '));
+  const topHeadlines = headlineElementsArr.slice(0,length).map(e => e.textContent);
+  const headlinesString = JSON.stringify(topHeadlines.join('. '));
 
-  const analysisParams = {
-    FunctionName: 'scrape-the-news-dev-analyser',
-    InvocationType: "RequestResponse",
-    Payload: headlinesStrng
-  };
+  const comprehendResult = await comprehend.detectSentiment({Text: headlinesString, LanguageCode: "en"}).promise();
+  const analysisResult = {Sentiment: comprehendResult.Sentiment, SentimentScore: comprehendResult.SentimentScore};
 
-  const result = await lambda.invoke(analysisParams).promise();
-  const resultObj = JSON.parse(result.Payload);
-  const analysisResult = {Sentiment: resultObj.Sentiment, SentimentScore: resultObj.SentimentScore};
-
-  let d = new Date();
-  let isoDate = d.toISOString();
+  const d = new Date();
+  const isoDate = d.toISOString();
 
   const dbParams = {
     Item: {
