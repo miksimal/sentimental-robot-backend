8a97e4cda53880919067276c664c82111d429e8c
Mikkel Lauritzen <mikkel.hlauritzen@gmail.com>
2020-06-02 08:54:17 +0100

    cleanup

--- handler.js
+++ handler.js
@@ -5,34 +5,6 @@ const { JSDOM } = jsdom;
 const AWS = require('aws-sdk');
 const client = new AWS.DynamoDB.DocumentClient({region: 'eu-west-1'});
 
-module.exports.scraper = async event => {
-  const URL = 'https://dr.dk';
-  const response = await got(URL);
-  const dom = new JSDOM(response.body);
-  const MAX_LENGTH = 10;
-
-  const headlineElements = dom.window.document.querySelectorAll("h2 > strong");
-  const headlineElementsArr = [...headlineElements];
-  const length = headlineElements.length >= MAX_LENGTH ? MAX_LENGTH : headlineElements.length;
-
-  let topHeadlines = headlineElementsArr.slice(0,length).map(e => e.textContent);
-
-  let d = new Date();
-  let isoDate = d.toISOString();
-
-  const params = {
-    Item: {
-      year: d.getFullYear(),
-      date: isoDate,
-      topHeadlines: topHeadlines,
-    },
-    TableName: process.env.HEADLINES_TABLE
-  };
-
-  return client.put(params).promise();
-
-}
-
 module.exports.bbcScraper = async event => {
   const lambda  = new AWS.Lambda({region: 'eu-west-1'});
   const URL = 'https://bbc.co.uk';
@@ -71,5 +43,4 @@ module.exports.bbcScraper = async event => {
   };
 
   return client.put(dbParams).promise();
-
 }
